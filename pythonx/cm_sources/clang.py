2fc120de95563306964cce32e8cd7ef54ee0ed3a
fcying <fcyingmk2@gmail.com>
2018-06-24 13:32:27 +0800

    add ncm deoplete source

---
+++ pythonx/cm_sources/clang.py
@@ -0,0 +1,182 @@
+# -*- coding: utf-8 -*-
+
+# =============================================================================
+# Forked from https://github.com/roxma/ncm-clang
+# =============================================================================
+
+# For debugging, use this command to start neovim:
+#
+# NVIM_PYTHON_LOG_FILE=nvim.log NVIM_PYTHON_LOG_LEVEL=INFO nvim
+#
+#
+# Please register source before executing any other code, this allow cm_core to
+# read basic information about the source without loading the whole module, and
+# modules required by this module
+from cm import register_source, getLogger, Base
+
+register_source(name='clang',
+                priority=9,
+                abbreviation='',
+                scoping=True,
+                scopes=['cpp', 'c'],
+                early_cache=1,
+                cm_refresh_patterns=[r'-\>', r'::', r'\.'],)
+
+import subprocess
+import re
+from os import path
+from ncm_clang import args_from_cmake, args_from_clang_complete
+
+logger = getLogger(__name__)
+
+
+class Source(Base):
+
+    def __init__(self, nvim):
+        super(Source, self).__init__(nvim)
+
+        self._clang_path = 'clang'
+
+    def cm_refresh(self, info, ctx, *args):
+
+        src = self.get_src(ctx).encode('utf-8')
+        lnum = ctx['lnum']
+        col = ctx['col']
+        filepath = ctx['filepath']
+        startcol = ctx['startcol']
+        ctx2 = self.nvim.eval('ncm_clang#_context()')
+        cwd = ctx2['cwd']
+        database_paths = ctx2['database_paths']
+        filedir = path.dirname(filepath)
+
+        scope = ctx['scope']
+        if scope=='cpp':
+            lang = 'c++'
+        else:
+            lang = 'c'
+
+        run_dir = cwd
+
+        args = [ self._clang_path,
+                '-x', lang,
+                '-fsyntax-only',
+                '-Xclang', '-code-completion-macros',
+                # '-Xclang', '-code-completion-brief-comments',
+                # Perfer external snippets instead of -code-completion-patterns
+                # '-Xclang', '-code-completion-patterns',
+                '-Xclang', '-code-completion-at={}:{}:{}'.format('-', lnum, col),
+                '-',
+                '-I', filedir
+                ]
+
+        cmake_args, directory = args_from_cmake(filepath, cwd, database_paths)
+        if cmake_args is not None:
+            args += cmake_args
+            run_dir = directory
+        else:
+            clang_complete_args, directory = args_from_clang_complete(filepath, cwd)
+            if clang_complete_args:
+                args += clang_complete_args
+                run_dir = directory
+
+        # args = args + self._clang_opts
+        logger.debug("args: %s", args)
+
+        proc = subprocess.Popen(args=args,
+                                stdin=subprocess.PIPE,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.DEVNULL,
+                                cwd=run_dir)
+
+        result, errs = proc.communicate(src, timeout=30)
+
+        result = result.decode()
+
+        logger.debug("result: [%s]", result)
+
+        matches = []
+
+        for line in result.split("\n"):
+            # COMPLETION: cout : [#ostream#]cout
+            # COMPLETION: terminate : [#void#]terminate()
+            if not line.startswith("COMPLETION: "):
+                continue
+            
+            try:
+                m = self.parse_completion(line)
+                matches.append(m)
+            except Exception as ex:
+                logger.exception("failed parsing completion: %s", line)
+
+        logger.debug("startcol: %s, matches: %s", startcol, matches)
+
+        self.complete(info, ctx, startcol, matches)
+
+    def parse_completion(self, line):
+        m = re.search(r'^COMPLETION:\s+([\w~&=!*/_]+)(\s+:\s+(.*)$)?', line)
+        word = m.group(1)
+
+        menu = ''
+        snippet = ''
+        is_snippet = False
+        snippet_num = 0
+
+        if m.group(3):
+            # [#double#]copysign(<#double __x#>, <#double __y#>)
+            more = m.group(3)
+            menu = re.sub(r'\[#([^#]+)#\]', r'\1 ', more)
+            menu = menu.replace('<#', '')
+            menu = menu.replace('#>', '')
+            menu = menu.replace('{#', '[')
+            menu = menu.replace('#}', ']')
+
+            if more.find('()') >= 0:
+                is_snippet = True
+
+            # function without parameter
+            if more.endswith('()'):
+                is_snippet = True
+
+            def rep(m):
+                nonlocal is_snippet
+                nonlocal snippet_num
+                is_snippet = True
+                snippet_num += 1
+                name = m.group(1)
+                last_word =re.search('([\w_]+)$', name)
+                if last_word:
+                    name = last_word.group(1)
+                return self.snippet_placeholder(snippet_num, name)
+
+            snippet = re.sub(r'\[#([^#]+)#\]', r'', more)
+            snippet = re.sub(r'\<#([^#]+)#\>', rep, snippet)
+
+            begin = None
+            end = None
+            mb = re.search('\<#', more)
+            me = re.search('.*#\>', more) # greedy
+            if mb:
+                begin = mb.start()
+            if me:
+                end = mb.end()
+
+            opt_begin = None
+            opt_end = None
+            mob = re.search('\{#', more)
+            moe = re.search('.*#\}', more)
+            if mob:
+                opt_begin = mob.start()
+            if moe:
+                opt_end = moe.end()
+
+            if opt_begin:
+                if opt_begin < begin and opt_end > end:
+                    snippet = re.sub(r'\{#.*#\}', self.snippet_placeholder(1), snippet)
+                else:
+                    snippet = re.sub(r'\{#.*#\}', '', snippet)
+
+        if is_snippet:
+            return dict(word=word, menu=menu, snippet=snippet)
+        else:
+            return dict(word=word, menu=menu)
+
---
+++ rplugin/python3/deoplete/source/clangx.py
@@ -0,0 +1,146 @@
+# =============================================================================
+# Forked from https://github.com/Shougo/deoplete-clangx
+# =============================================================================
+
+import re
+import os.path
+from os.path import expanduser, expandvars, dirname
+import subprocess
+import shlex
+from itertools import chain
+
+from deoplete.util import getlines, error
+from .base import Base
+
+
+class Source(Base):
+    run_dir = ''
+
+    def __init__(self, vim):
+        Base.__init__(self, vim)
+
+        self.name = 'clangx'
+        self.filetypes = ['c', 'cpp']
+        self.mark = '[clangx]'
+        self.rank = 500
+        self.executable_clang = self.vim.call('executable', 'clang')
+        self.encoding = self.vim.eval('&encoding')
+        self.input_pattern = r'\.[a-zA-Z0-9_?!]*|[a-zA-Z]\w*::\w*|->\w*'
+        self.vars = {
+            'clang_binary': 'clang',
+            'default_c_options': '',
+            'default_cpp_options': '',
+        }
+
+        self._args = []
+
+    def on_event(self, context):
+        self._args = self._args_from_neoinclude(context)
+
+        Source.run_dir = context['cwd']
+        clang = self._args_from_clang(context, '.clang')
+        clang += self._args_from_clang(context, '.clang_complete')
+        if clang:
+            self._args += clang
+        else:
+            self._args += (self.vars['default_cpp_options']
+                           if context['filetype'] == 'cpp'
+                           else self.vars['default_c_options'])
+
+    def get_complete_position(self, context):
+        m = re.search('[a-zA-Z0-9_]*$', context['input'])
+        return m.start() if m else -1
+
+    def gather_candidates(self, context):
+        if not self.executable_clang:
+            return []
+
+        line = context['position'][1]
+        column = context['complete_position'] + 1
+        lang = 'c++' if context['filetype'] == 'cpp' else 'c'
+        buf = '\n'.join(getlines(self.vim)).encode(self.encoding)
+
+        args = [
+            self.vars['clang_binary'],
+            '-x', lang, '-fsyntax-only',
+            '-Xclang', '-code-completion-macros',
+            '-Xclang', '-code-completion-at=-:{}:{}'.format(line, column),
+            '-',
+            '-I', os.path.dirname(context['bufpath']),
+        ]
+        args += self._args
+
+        try:
+            proc = subprocess.Popen(args=args,
+                                    stdin=subprocess.PIPE,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.DEVNULL,
+                                    cwd=Source.run_dir)
+            result, errs = proc.communicate(buf, timeout=10)
+            result = result.decode(self.encoding)
+        except subprocess.TimeoutExpired as e:
+            proc.kill()
+            return []
+
+        return self._parse_lines(result.splitlines())
+
+    def _args_from_neoinclude(self, context):
+        if not self.vim.call(
+                'exists', '*neoinclude#get_path'):
+            return []
+
+        # Make cache
+        self.vim.call('neoinclude#include#get_include_files')
+
+        return list(chain.from_iterable(
+            [['-I', x] for x in
+             self.vim.call('neoinclude#get_path',
+                           context['bufnr'],
+                           context['filetype']).replace(';', ',').split(',')
+             if x != '']))
+
+    def _args_from_clang(self, context, name):
+        clang_file = self.vim.call('findfile', name, '.;')
+        if not clang_file:
+            return []
+
+        clang_file = self.vim.call('fnamemodify', clang_file, ':p')
+
+        try:
+            with open(clang_file) as f:
+                args = shlex.split(' '.join(f.readlines()))
+                args = [expanduser(expandvars(p)) for p in args]
+                Source.run_dir = dirname(clang_file)
+                return args
+        except Exception as e:
+            error(self.vim, 'Parse Failed: ' + clang_file)
+        return []
+
+    def _parse_lines(self, lines):
+        candidates = []
+        for line in lines:
+            m = re.search('^COMPLETION:\s+(.{,}?) : (.{,}?)$', line)
+            if not m:
+                m = re.search('^COMPLETION:\s+(.*)$', line)
+                if m:
+                    candidates.append({'word': m.group(1)})
+                continue
+
+            menu = m.group(2)
+            menu = menu.replace('[#', '')
+            menu = menu.replace('#]', ' ')
+            menu = menu.replace('<#', '')
+            menu = menu.replace('#>', '')
+            menu = menu.replace('{#', '')
+            menu = menu.replace('#}', '')
+
+            word = m.group(1)
+            if word.startswith('PFNG'):
+                continue
+
+            candidate = {'word': word, 'dup': 1}
+            if menu != word:
+                candidate['menu'] = menu
+                candidate['info'] = menu
+            candidates.append(candidate)
+        return candidates
